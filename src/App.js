faddd186b5c78c75d98ba37bc041e865d524e132
votantai <taivo.it2210@gmail.com>
2022-08-28 09:40:36 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useRef, useState } from "react";
+import aalib from "aalib.js";
 
 function App() {
+  const videoRef = useRef(null);
+  const [videoSrc, setVideoSrc] = useState("");
+
+  const initAalib = useCallback(() => {
+    const videoWidth = document.querySelector("video").offsetWidth;
+    const videoHeight = document.querySelector("video").offsetHeight;
+    const aspectRatio = {
+      width: Math.round(videoWidth / 4),
+      height: Math.round(videoHeight / 8),
+    };
+    console.log(aspectRatio, "aspectRatio");
+    aalib.read.video
+      .fromVideoElement(document.querySelector("video"))
+      .map(aalib.aa(aspectRatio))
+      .map(
+        aalib.render.canvas({
+          width: videoWidth,
+          height: videoHeight,
+          el: document.querySelector("#video-scene"),
+        })
+      )
+      .subscribe();
+  }, []);
+
+  const getBase64 = useCallback((file, callback) => {
+    const reader = new FileReader();
+    reader.addEventListener("load", () => callback(reader.result));
+    reader.readAsDataURL(file);
+  }, []);
+
+  const handleChange = (event) => {
+    getBase64(event.target.files[0], (base64) => {
+      setVideoSrc(() => base64);
+      if (!videoRef.current.className.includes("h-screen")) {
+        videoRef.current.className = "h-screen opacity-0";
+      }
+    });
+    setTimeout(() => {
+      initAalib();
+      videoRef.current.play();
+    }, 200);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex">
+      <input type="file" name="file" onChange={handleChange} />
+      <canvas id="video-scene" />
+      <video
+        loop
+        controls
+        src={videoSrc}
+        className="opacity-0"
+        ref={videoRef}
+      />
     </div>
   );
 }
